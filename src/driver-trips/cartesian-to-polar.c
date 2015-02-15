123cb830e8acf1214299508bd74e05f8489d28ce
William Stevenson <william@skybluecircles.com>
2015-02-15 15:26:55 -0500

    Looping around all args

--- src/driver-trips/cartesian-to-polar.c
+++ src/driver-trips/cartesian-to-polar.c
@@ -3,18 +3,22 @@
 #include <string.h>
 #include <math.h>
 
-#define PATHMAX 256 
-#define LINEMAX 256
+#define MAX_ARGS 32767
+#define MAX_ARG_LENGTH 8
+#define MAX_PATH_LENGTH 256
+#define MAX_LINE_LENGTH 256
 #define IFS ","
 
-int main() {
-    char in[PATHMAX];
-    char out[PATHMAX];
+int main( int argc, char *argv[] ) {
+    int  i;
+
+    char in[MAX_PATH_LENGTH];
+    char out[MAX_PATH_LENGTH];
 
     FILE *ifp;
     FILE *ofp;
 
-    char line[LINEMAX];
+    char line[MAX_LINE_LENGTH];
 
     float x;
     float y;
@@ -22,48 +26,87 @@ int main() {
     float radius;
     float azimuth;
 
-    int driver_id = 1;
+    char driver_id[MAX_ARG_LENGTH];
     int trip_id;
 
-    for ( trip_id = 1; trip_id <= 200; trip_id++ ) {
-        snprintf(
-          in, PATHMAX,
-          "../data/driver/%d/trip/%d/coordinates-cartesian",
-          driver_id, trip_id
+    int  path_length;
+    char ENV_VAR[MAX_LINE_LENGTH];
+    char DTA_DATA[MAX_LINE_LENGTH];
+
+    snprintf( ENV_VAR, MAX_LINE_LENGTH, "%s", "DTA_DATA" );
+    path_length = strlen(getenv(ENV_VAR));
+
+    if ( path_length > MAX_PATH_LENGTH ) {
+        fprintf(
+            stderr,
+            "You have too many characters (%d) in the enviroment variable DTA_DATA. The limit is (%d).",
+            path_length, MAX_PATH_LENGTH
         );
+        exit(1);
+    }
+    else {
+        snprintf( DTA_DATA, MAX_PATH_LENGTH, "%s", getenv(ENV_VAR) );
+    }
 
-        snprintf(
-          out, PATHMAX,
-          "../data/driver/%d/trip/%d/coordinates-polar",
-          driver_id, trip_id
-        );
+    if ( argc < 2 ) {
+        fprintf( stderr, "Usage: %s 1 [2 [ n ] ]\n", argv[0] );
+        exit(1);
+    }
+    else if ( argc > MAX_ARGS ) {
+        fprintf( stderr, "Too many arguments (%d). Limit is (%d)\n", argc, MAX_ARGS );
+        exit(1);
+    }
 
-        ifp = fopen( in, "r" );
-        ofp = fopen( out, "w" );
-
-        if ( ifp == NULL ) {
-            fprintf( stderr, "Can't open %s\n", in );
+    for ( i = 1; i < argc; i++ ) {
+        if ( strlen(argv[i]) > MAX_ARG_LENGTH ) {
+            fprintf( stderr, "arg %d (%s) has more than %d characters\n", i, argv[i], MAX_ARG_LENGTH );
             exit(1);
         }
+    }
 
-        if ( ofp == NULL ) {
-            fprintf( stderr, "Can't open %s\n", out );
-            exit(1);
+    for ( i = 1; i < argc; i++ ) {
+        snprintf( driver_id, MAX_ARG_LENGTH, "%s", argv[i] );
+
+        for ( trip_id = 1; trip_id <= 200; trip_id++ ) {
+            snprintf(
+              in, MAX_PATH_LENGTH,
+              "%s/driver/%s/trip/%d/coordinates-cartesian",
+              DTA_DATA, driver_id, trip_id
+            );
+
+            snprintf(
+              out, MAX_PATH_LENGTH,
+              "../data/driver/%s/trip/%d/coordinates-polar-c2",
+              driver_id, trip_id
+            );
+
+            ifp = fopen( in, "r" );
+            ofp = fopen( out, "w" );
+
+            if ( ifp == NULL ) {
+                fprintf( stderr, "Can't open %s\n", in );
+                exit(1);
+            }
+
+            if ( ofp == NULL ) {
+                fprintf( stderr, "Can't open %s\n", out );
+                exit(1);
+            }
+
+            while ( fgets( line, MAX_LINE_LENGTH, ifp ) != NULL ) {
+                line[ strcspn( line, "\n") ] = 0;
+
+                x = atof( strtok( line, IFS ) );
+                y = atof( strtok( NULL, IFS ) );
+
+                radius = sqrt(pow(x,2)+pow(y,2));
+                azimuth = atan2(y,x);
+
+                fprintf( ofp, "%f,%f\n", radius, azimuth );
+            }
+
+            fclose(ifp);
+            fclose(ofp);
         }
-
-        while ( fgets( line, LINEMAX, ifp ) != NULL ) {
-            line[ strcspn( line, "\n") ] = 0;
-
-            x = atof( strtok( line, IFS ) );
-            y = atof( strtok( NULL, IFS ) );
-
-            radius = sqrt(pow(x,2)+pow(y,2));
-            azimuth = atan2(y,x);
-
-            fprintf( ofp, "%f,%f\n", radius, azimuth );
-        }
-
-        fclose(ifp);
-        fclose(ofp);
     }
 }
