cfa5508bf284d46d60d4773eb271f59687cb7daa
William Stevenson <william@skybluecircles.com>
2015-03-08 18:38:30 -0400

    Left justified title

--- bin/plot/util/feature-box-plots.R
+++ bin/plot/util/feature-box-plots.R
@@ -9,7 +9,9 @@ features        <- subset( features.tagged, select = -c( driver_id, trip_id ) )
 out <- driver_file( driver_id, "box-plots.svg" )
 svg(out)
 
-title <- paste( "Boxplots for Driver", driver_id, "Features", sep = " " )
-boxplot( features, xlab = "", main = title, las = 2 )
+boxplot( features, xlab = "", las = 2 )
+
+title <- paste( "Boxplots for Driver", driver_id, "Features               no scaling", sep = " " )
+title( title, adj = 0 )
 
 dev.off()
--- bin/plot/util/feature-box-plots.min-max-norm.R
+++ bin/plot/util/feature-box-plots.min-max-norm.R
@@ -6,10 +6,12 @@ source("bin/util/driver-trip-args.R")
 features.tagged <- driver_csv( driver_id, "features.min-max-norm.csv" )
 features        <- subset( features.tagged, select = -c( driver_id, trip_id ) )
 
-out <- driver_file( driver_id, "box-plots.min-max-norm.svg" )
+out <- driver_file( driver_id, "box-plots-min-max-norm.svg" )
 svg(out)
 
-title <- paste( "Boxplots for Driver", driver_id, "Features (Min/Max Normalized)", sep = " " )
-boxplot( features, xlab = "", main = title, las = 2 )
+boxplot( features, xlab = "", las = 2 )
+
+title <- paste( "Boxplots for Driver", driver_id, "Features               min/max normalized", sep = " " )
+title( title, adj = 0 ) # left just so less change when user toggles between this and "no scaling"
 
 dev.off()
